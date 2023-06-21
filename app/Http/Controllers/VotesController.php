8fc3370d6f5881f76dc930dc5f9a80d7e51c0fca
andrey nascimento <candreynascimento@gmail.com>
2023-06-21 00:18:27 -0300

    feat: store votes

--- app/Http/Controllers/VotesController.php
+++ app/Http/Controllers/VotesController.php
@@ -2,8 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Votes\StoreVotesRequest;
+use App\Models\Votes;
+use Exception;
 use Illuminate\Http\Request;
 
 class VotesController extends Controller
 {
+    function store(StoreVotesRequest $request)
+    {
+        try {
+            $params = $request->all();
+
+            $vote = Votes::create($params);
+
+            return response()->json(['created' => true], 200);
+        } catch (Exception $e) {
+            return response()->json($e->getMessage(), $e->getCode() || 500);
+        }
+    }
 }

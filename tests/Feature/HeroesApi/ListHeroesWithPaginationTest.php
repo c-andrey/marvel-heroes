16cba582dc964eb4a77c283e4578fde9f34ef351
andrey nascimento <candreynascimento@gmail.com>
2023-06-21 18:46:23 -0300

    test: list heroes with pagination passing page and perPage

---
+++ tests/Feature/HeroesApi/ListHeroesWithPaginationTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Feature\HeroesApi;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Http;
+use Tests\TestCase;
+
+class ListHeroesWithPaginationTest extends TestCase
+{
+    /**
+     * Basic pagination test on heroes api.
+     */
+    public function test_list_heroes_with_pagination(): void
+    {
+        $page = 2;
+        $perPage = 5;
+        $offset = ($page - 1) * $perPage;
+        $url = config('MARVEL_API_URL') . '/characters*';
+        Http::fake([
+            $url => Http::response([
+                'data' => [
+                    "offset" =>  $offset,
+                    "limit" =>  5,
+                    "total" =>  1562,
+                    "count" =>  5,
+                    "results" =>  [
+                        [
+                            "id" =>  1011334,
+                            "name" =>  "3-D Man"
+                        ],
+                        [
+                            "id" =>  1011334,
+                            "name" =>  "3-D Man"
+                        ],
+                        [
+                            "id" =>  1011334,
+                            "name" =>  "3-D Man"
+                        ],
+                        [
+                            "id" =>  1011334,
+                            "name" =>  "3-D Man"
+                        ],
+                        [
+                            "id" =>  1011334,
+                            "name" =>  "3-D Man"
+                        ]
+                    ]
+                ]
+            ], 200)
+        ]);
+
+
+        $response = $this->get('/api/heroes' . '?page=' . $page . '&perPage=' . $perPage);
+
+        $response->assertStatus(200);
+        $response->assertJsonCount(5, 'heroes.results');
+        $response->assertJson([
+            'heroes' => [
+                'offset' => 5,
+                'page' => 2
+            ]
+        ]);
+    }
+}

4991b4eea929c16efd92666d56e9d170ad99b838
andrey nascimento <candreynascimento@gmail.com>
2023-06-21 00:37:34 -0300

    test: update up votes based on hero id

--- tests/Feature/UpdateVotesTest.php
+++ tests/Feature/UpdateVotesTest.php
@@ -2,31 +2,31 @@
 
 namespace Tests\Feature;
 
-use App\Models\Votes;
+use Database\Seeders\VotesSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class StoreVotesTest extends TestCase
+class UpdateVotesTest extends TestCase
 {
     use RefreshDatabase;
     /**
-     * Test for create votes.
+     * A test to update votes.
      */
-    public function test_create_votes(): void
+    public function test_update_votes(): void
     {
+        $this->seed(VotesSeeder::class);
+
         $response = $this->post('/api/vote', [
-            'hero_id' => 1,
+            'hero_id' => 1011334,
             'action' => 'up'
         ]);
-
 
         $response->assertStatus(200);
         $response->assertJson(['voted' => true]);
         $this->assertDatabaseHas('votes', [
-            'hero_id' => 1,
-            'votes' => 0
+            'hero_id' => 1011334,
+            'votes' => 2
         ]);
-        $this->assertDatabaseCount('votes', 1);
     }
 }

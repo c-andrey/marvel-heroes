715a4fdf68c7dff6536f2a77288fe3d0ae8a44a2
andrey nascimento <candreynascimento@gmail.com>
2023-06-20 23:12:32 -0300

    feat: test to list heroes using marvel api

---
+++ app/Services/HeroesRemoteService/HeroesRemoteService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services\HeroesRemoteService;
+
+use Illuminate\Support\Facades\Http;
+
+class HeroesRemoteService
+{
+    public function getHeroes()
+    {
+        $response = Http::get(env('MARVEL_API_URL') . '/characters', [
+            'apikey' =>  env('MARVEL_API_PUBLIC_KEY'),
+            'ts' =>  env('MARVEL_API_TIMESTAMP'),
+            'hash' =>  env('MARVEL_API_HASHMD5')
+        ]);
+
+        if ($response->successful()) {
+            return response()->json($response->json()['data'], $response->status());
+        }
+
+        return response()->json([
+            'data' => null,
+            'message' => 'Failed',
+        ], 500);
+    }
+}
